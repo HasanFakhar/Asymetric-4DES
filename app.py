13161f4ee1418cd5cf75b1cc2380844570fb3cec
Hasan Fakhar <hasanfakhar@hotmail.com>
2024-12-22 20:25:07 +0500

    integrate RSA

--- app.py
+++ app.py
@@ -42,6 +42,8 @@ def four_des_decrypt(ciphertext, keys):
     pad_len = data[-1]
     data = data[:-pad_len]
     return data
+
+
 def compute_hmac(ciphertext, hmac_key):
     """Compute an HMAC-SHA256 over the given ciphertext."""
     h = HMAC.new(hmac_key, digestmod=SHA256)
@@ -53,3 +55,24 @@ def verify_hmac(ciphertext, hmac_key, expected_hmac):
     h = HMAC.new(hmac_key, digestmod=SHA256)
     h.update(ciphertext)
     h.verify(expected_hmac)  # Will raise ValueError on mismatch
+
+
+def wrap_keys_with_rsa(des_keys, hmac_key, rsa_public_key_pem):
+    """Encrypt 4 DES keys + 1 HMAC key using RSA public key."""
+    rsa_key = RSA.import_key(rsa_public_key_pem)
+    cipher_rsa = PKCS1_OAEP.new(rsa_key)
+    # Combine all keys into a single blob
+    combined = b''.join(des_keys) + hmac_key
+    encrypted_blob = cipher_rsa.encrypt(combined)
+    return encrypted_blob
+
+def unwrap_keys_with_rsa(encrypted_blob, rsa_private_key_pem):
+    """Decrypt (unwrap) keys using RSA private key."""
+    rsa_key = RSA.import_key(rsa_private_key_pem)
+    cipher_rsa = PKCS1_OAEP.new(rsa_key)
+    combined = cipher_rsa.decrypt(encrypted_blob)
+    # The last 32 bytes is the HMAC key (example length)
+    # The first 4 * 8 = 32 bytes are the four DES keys
+    four_des_keys = [combined[i*8:(i+1)*8] for i in range(4)]
+    hmac_key = combined[32:]  # next 32 bytes or however long you chose
+    return four_des_keys, hmac_key
